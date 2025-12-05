e5af970c88a2b57e0448fd04c5c30d8da97978ab
Ashking56 <218508386+PhoenixAshking@users.noreply.github.com>
2025-12-05 12:04:21 -1000

    nuevo intento parte 11

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 
 const morgan = require("morgan");
 const cors = require("cors");
-app.use(cors());
 app.use(express.static("dist"));
+
+app.use(cors());
 
 morgan.token("post-data", (req) => {
   if (req.method === "POST") return JSON.stringify(req.body);
